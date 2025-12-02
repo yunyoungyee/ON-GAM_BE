d3923324dd9731fb5931bf42ec7e245b985cf5bf
yunyoungyee <eric3725@naver.com>
2025-12-02 16:26:30 +0900

    fix : @Configuration 수정

--- src/main/java/com/example/spring_assignment1/config/PasswordEncoderConfig.java
+++ src/main/java/com/example/spring_assignment1/config/PasswordEncoderConfig.java
@@ -1,9 +1,11 @@
 package com.example.spring_assignment1.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class PasswordEncoderConfig {
 
     @Bean
