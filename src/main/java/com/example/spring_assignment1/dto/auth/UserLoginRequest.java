371a37bd3444f0363a686bfea93efaa2fcb31aa0
yunyoungyee <eric3725@naver.com>
2025-12-02 16:42:55 +0900

    refactor : DTO에 기본 생성자, Getter 추가
    ObjectMapper에서 DTO로 쓸때 기본생성자와 Getter를 쓰도록 개선

--- src/main/java/com/example/spring_assignment1/dto/auth/UserLoginRequest.java
+++ src/main/java/com/example/spring_assignment1/dto/auth/UserLoginRequest.java
@@ -2,14 +2,16 @@ package com.example.spring_assignment1.dto.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class UserLoginRequest {
     @Schema(description = "사용자 이메일", example = "eric@kakao.com")
     @Email
     private String email;
     @Schema(description = "사용자 비밀번호", example = "test1234")
     private String password;
-
-    public String getEmail() { return email; }
-    public String getPassword() { return password; }
 }
