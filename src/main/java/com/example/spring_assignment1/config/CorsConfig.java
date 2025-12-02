c6344ac8fb914532bd120115cc4df6c7c6992022
yunyoungyee <eric3725@naver.com>
2025-12-02 16:22:30 +0900

    feat : SecurityConfig 기본 구성
    세션 기반 인증 사용
    AuthenticationManager, Provider 등록
    CustomFilter 등록

---
+++ src/main/java/com/example/spring_assignment1/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.spring_assignment1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration= new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
--- src/main/java/com/example/spring_assignment1/config/SecurityConfig.java
+++ src/main/java/com/example/spring_assignment1/config/SecurityConfig.java
@@ -1,35 +1,95 @@
 package com.example.spring_assignment1.config;
 
+import com.example.spring_assignment1.security.filter.CustomLoginFilter;
+import com.example.spring_assignment1.security.handler.CustomAuthenticationFailureHandler;
+import com.example.spring_assignment1.security.handler.CustomAuthenticationSuccessHandler;
+import com.example.spring_assignment1.service.security.CustomUserDetailsService;
+import com.example.spring_assignment1.config.PasswordEncoderConfig;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfig corsConfig;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoderConfig passwordEncoderConfig;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, CustomLoginFilter customLoginFilter) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/signup", "/", "/login").permitAll()
-                        .anyRequest().authenticated()
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .loginProcessingUrl("/users/auth")
-                        .defaultSuccessUrl("/")
-                        .permitAll()
+                .securityContext(context->context
+                        .securityContextRepository(new  HttpSessionSecurityContextRepository()))
+
+                .addFilterAt(customLoginFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/images/**").permitAll()
+                        .requestMatchers("/users/auth","/users").permitAll()
+                        .requestMatchers("/users/**").authenticated()
+                        .requestMatchers("/posts/**","/comments/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().denyAll()
                 )
-                .logout((logout) -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                .logout(logout->logout
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
                 );
+
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoderConfig.passwordEncoder());
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    @Bean
+    public CustomLoginFilter customLoginFilter(
+            AuthenticationManager authenticationManager,
+            CustomAuthenticationSuccessHandler successHandler,
+            CustomAuthenticationFailureHandler failureHandler) {
+        CustomLoginFilter filter = new CustomLoginFilter(authenticationManager, successHandler, failureHandler);
+        return filter;
+    }
+
+
 }
